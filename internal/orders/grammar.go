905e57ef926fff99bcac28a83ce32863c5abc203
Michael Henderson <mdhender@mdhender.com>
2023-05-08 22:17:16 -0600

    updated grammar

--- internal/orders/grammar.go
+++ internal/orders/grammar.go
@@ -7,25 +7,25 @@ package orders
 const Grammar = `
 	orders = {order | EOL} EOF .
 
-	order = assemble | bombard | invade | raid | setup | support .
+	order = assemble | bombard | invade | raid | retool | setup | support .
 
 	assemble    = "assemble"    CSID [DEPOSITID | FACTGRP | MINEGRP] QUANTITY material EOL .
 	disassemble = "disassemble" CSID [            FACTGRP | MINEGRP] QUANTITY material EOL .
 	retool      = "retool"      CSID FACTGRP material EOL .
-	material    = "research" | PRODUCT .
 
 	bombard  = "bombard"  CSID CSID        PERCENTAGE       EOL .
 	invade   = "invade"   CSID CSID        PERCENTAGE       EOL .
 	raid     = "raid"     CSID CSID        PERCENTAGE cargo EOL .
 	support  = "support"  CSID CSID [CSID] PERCENTAGE       EOL .
 
-	transfer = "transfer" CSID QUANTITY cargo CSID EOL .  
-	cargo = POPULATION | PRODUCT | RESEARCH | RESOURCE .
+	transfer = "transfer" CSID QUANTITY cargo CSID EOL .
 
 	setup    = "setup"    CSID coordinate ("ship" | "colony") "transfer" EOL
 	           {xfer_detail}
 	           "end" EOL .
 
+	cargo       = POPULATION | PRODUCT | RESEARCH | RESOURCE .
 	coordinate  = PARENOP INTEGER COMMA INTEGER COMMA INTEGER [COMMA INTEGER] PARENCL .
+	material    = "research" | PRODUCT .
 	xfer_detail = QUANTITY cargo TEXT EOL .
 `
